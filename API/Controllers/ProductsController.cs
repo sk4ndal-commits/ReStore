b72498cd5817016262bf4628ccefa141bd415509
sk4ndal-commits <sascharoggatz1999@outlook.de>
2023-02-03 21:21:20 +0100

    inherit from BaseApiController now

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,28 +5,29 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+namespace API.Controllers
 {
-    private  readonly StoreContext _context;
+
+    public class ProductsController : BaseApiController
+    {
+        private  readonly StoreContext _context;
     
-    public ProductsController(StoreContext context)
-    {
-        _context = context;
-    }
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
-    {
-        return await _context.Products.ToListAsync();
-    }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
 
-    [HttpGet("{id:int}")]
-    public async Task<ActionResult<Product>> GetProduct(int id)
-    {
-        return await _context.Products.FindAsync(id);
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product == null ? NotFound() : product;
+        }
     }
 }
